03203f1df1c0c1e0d5e9602d3abeb878be839397
Ruhi Awasthi <ruhi.awasthi@incedoinc.com>
2024-04-26 13:50:15 +0530

    Laying the foundation. Added jsx

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM from "react-dom/client";
 // always use keys if you have more then one child
-const heading1 = React.createElement("h1", { id: "title", key:"h1" }, "Heading1");
-const heading2 = React.createElement("h2", { id: "title" , key:"h2"}, "Heading2");
+const heading1 = React.createElement(
+  "h1",
+  { id: "title", key: "h1" },
+  "Heading1"
+);
+const heading2 = React.createElement(
+  "h2",
+  { id: "title", key: "h2" },
+  "Heading2"
+);
 const container = React.createElement("div", { id: "container" }, [
   heading1,
   heading2,
 ]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+// root.render(container);
+
+// React element
+const jsxHeading = (
+  <div>
+    <h1>jsxHeading</h1>
+  </div>
+);
+
+// root.render(jsxHeading);
+
+// React componenet
+const Jsxcomponenet =()=> (
+  <div>
+    <h1>jsxcomponenet</h1>
+  </div>
+);
+
+
+const Jsxcomponenet1 =()=> (
+  <div>
+    {jsxHeading}
+    <Jsxcomponenet/>
+    <h1>jsxcomponenet1</h1>
+  </div>
+);
+root.render(<Jsxcomponenet1/>);
+
+
